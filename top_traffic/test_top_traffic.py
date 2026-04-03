107a6ad1bee1e2444293b35016915fbe410fa8f9
Shashwat Singh Chauhan <144317840+Shashwat-Chauhan@users.noreply.github.com>
2026-04-04 00:25:00 +0530

    Delete top_traffic/test_top_traffic.py

--- top_traffic/test_top_traffic.py
+++
@@ -1,9 +0,0 @@
-def test_rate_calculation():
-    prev = {"Eth0": {"rx_bytes": 1000, "tx_bytes": 2000}}
-    curr = {"Eth0": {"rx_bytes": 3000, "tx_bytes": 4000}}
-
-    from top_traffic.traffic_calculator import TrafficCalculator
-
-    rates = TrafficCalculator.calculate_rate(prev, curr, 2)
-
-    assert rates["Eth0"]["total"] > 0
